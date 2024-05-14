2f98af6f7dfa539e1a883351bb08be7563587344
koiescafe2024 <168373038+koiescafe2024@users.noreply.github.com>
2024-05-14 23:58:05 +0530

    env

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: "dotbet-frontend",
+      name: "queen-frontend",
       script: "npm start", // Replace with your main application file
       instances: 1,
       autorestart: true,
